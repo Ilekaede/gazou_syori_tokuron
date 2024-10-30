b2b76f88412a389900f95b0dd03cc7ecdb6dbe7b
Ilekaede <asakuramotiyo@gmail.com>
2024-10-30 23:19:58 +0900

    第2回講義のプログラムを作成

---
+++ 2/main.py
@@ -0,0 +1,12 @@
+import cv2
+
+img = cv2.imread('../img/lenna.bmp')
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+_, dst = cv2.threshold(img, 0, 255, cv2.THRESH_OTSU)
+
+cv2.imshow('binary', dst)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# cv2.imwrite('./result.png', dst)
